5873a11b5ea860f77cfd94295114f8733100959f
Colin Valentini <valentini.colin@gmail.com>
2022-01-31 19:45:55 -0500

    fixup! leetcode: add solution to find words that can be formed by characters problem

--- practice/problems/leetcode/find-words-that-can-be-formed-by-characters/find_words_that_can_be_formed_by_characters_test.go
+++ practice/problems/leetcode/find-words-that-can-be-formed-by-characters/find_words_that_can_be_formed_by_characters_test.go
@@ -9,8 +9,8 @@ import (
 // TestCountCharacters runs test cases for CountCharacters.
 func TestCountCharacters(t *testing.T) {
 	assert := assert.New(t)
-	// assert.Equal(6, CountCharacters([]string{"cat", "bt", "hat", "tree"}, "atach"))
-	// assert.Equal(10, CountCharacters([]string{"hello", "world", "leetcode"}, "welldonehoneyr"))
+	assert.Equal(6, CountCharacters([]string{"cat", "bt", "hat", "tree"}, "atach"))
+	assert.Equal(10, CountCharacters([]string{"hello", "world", "leetcode"}, "welldonehoneyr"))
 	assert.Equal(0, CountCharacters(
 		[]string{
 			"dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin", 
