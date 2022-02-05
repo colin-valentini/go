ecbd0a4df536b46245c60c7e9e999c6d38678194
Colin Valentini <valentini.colin@gmail.com>
2022-02-05 11:01:49 -0500

    leetcode: add solution to longest palindromic substring problem
    Adds a Go solution to the "Longest Palindromic Substring" problem.

---
+++ practice/problems/leetcode/longest-palindromic-substring/longest_palindromic_substring_test.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLongestPalindrome(t *testing.T) {
+	assert.Equal(t, "d", longestPalindrome("d"))
+	assert.Equal(t, "bab", LongestPalindrome("babad"))
+	assert.Equal(t, "bb", longestPalindrome("cbbd"))
+	assert.Equal(t, "bdb", longestPalindrome("abbdb"))
+	assert.Equal(t, "abbba", LongestPalindrome("babbbad"))
+	assert.Equal(t, "aaa", longestPalindrome("aaadb"))
+	assert.Equal(t, "bbbb", longestPalindrome("abbbbc"))
+	assert.Equal(t, "ababababa", longestPalindrome("ababababa"))
+}
