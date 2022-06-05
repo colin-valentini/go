68b3e267ed3aafb442bc245c667531328252fbd9
Colin Valentini <valentini.colin@gmail.com>
2022-06-05 17:15:13 -0400

    add another test case

--- practice/leetcode/207_course-schedule/solution_test.go
+++ practice/leetcode/207_course-schedule/solution_test.go
@@ -9,4 +9,10 @@ import (
 func TestCanFinish(t *testing.T) {
 	assert.Equal(t, true, CanFinish(2, [][]int{{1, 0}}), "Example 1")
 	assert.Equal(t, false, CanFinish(2, [][]int{{1, 0}, {0, 1}}), "Example 2")
+	assert.Equal(t, true, CanFinish(4, [][]int{
+		{1, 0},
+		{3, 0},
+		{3, 1},
+		{3, 2},
+	}))
 }
