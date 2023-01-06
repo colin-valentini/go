3a22b0335696e6b35b80a04d45b546d6c9dd804d
Colin Valentini <valentini.colin@gmail.com>
2023-01-05 19:38:58 -0500

    leetcode: confusing number

--- practice/leetcode/1056_confusing-number/solution_test.go
+++ practice/leetcode/1056_confusing-number/solution_test.go
@@ -7,16 +7,18 @@ import (
 )
 
 func TestSolver(t *testing.T) {
-    testCases := []struct{
-        // TODO
-        in any
-        want any
-    }{
-        // TODO
-    }
-    for i, testCase := range testCases {
-        solver := NewSolver(testCase.in)
-        got := solver.Solve()
-        assert.Equal(t, testCase.want, got, "Failed test case %d", i+1)
-    }
+	testCases := []struct {
+		n    int
+		want bool
+	}{
+		{n: 6, want: true},
+		{n: 89, want: true},
+		{n: 11, want: false},
+		{n: 916, want: false},
+	}
+	for i, testCase := range testCases {
+		solver := NewSolver(testCase.n)
+		got := solver.Solve()
+		assert.Equal(t, testCase.want, got, "Failed test case %d", i+1)
+	}
 }
