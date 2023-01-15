59080c08c8133277d349f23686af81fb967547fb
Colin Valentini <valentini.colin@gmail.com>
2023-01-15 15:24:45 -0500

    fix code coverage

--- practice/leetcode/240_search-a-2d-matrix-ii/solution_test.go
+++ practice/leetcode/240_search-a-2d-matrix-ii/solution_test.go
@@ -34,6 +34,40 @@ func TestSolver(t *testing.T) {
 			target: 20,
 			want:   false,
 		},
+		{
+			matrix: [][]int{
+				{1, 4, 7, 11, 15},
+				{2, 5, 8, 12, 19},
+				{3, 6, 9, 16, 22},
+				{10, 13, 14, 17, 24},
+				{18, 21, 23, 26, 30},
+			},
+			target: 0,
+			want:   false,
+		},
+		{
+			matrix: [][]int{
+				{1, 4, 7, 11, 15},
+				{2, 5, 8, 12, 19},
+				{3, 6, 9, 16, 22},
+				{10, 13, 14, 17, 24},
+				{18, 21, 23, 26, 30},
+			},
+			target: 69,
+			want:   false,
+		},
+		{
+			matrix: [][]int{},
+			target: 1,
+			want:   false,
+		},
+		{
+			matrix: [][]int{
+				{},
+			},
+			target: 1,
+			want:   false,
+		},
 	}
 	for i, testCase := range testCases {
 		solver := NewSolver(testCase.matrix, testCase.target)
