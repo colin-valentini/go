a15858dfbb9a667406a16be9a25ed1b698dae5b7
Colin Valentini <valentini.colin@gmail.com>
2023-02-26 14:53:08 -0500

    leetcode: rotting oranges

---
+++ practice/leetcode/994_rotting-oranges/solution.go
@@ -0,0 +1,105 @@
+package rottingoranges
+
+// LeetCode #994.
+// https://leetcode.com/problems/rotting-oranges/
+
+type Solver struct {
+	m, n int
+	grid [][]int
+}
+
+func NewSolver(grid [][]int) *Solver {
+	return &Solver{m: len(grid), n: len(grid[0]), grid: grid}
+}
+
+func (s *Solver) Solve() int {
+	q := newQueue()
+	freshCount := 0
+	for r := 0; r < s.m; r++ {
+		for c := 0; c < s.n; c++ {
+			cell := loc{r: r, c: c}
+			if v := s.value(cell); v == rotten {
+				q.enqueue(cell)
+			} else if v == fresh {
+				freshCount++
+			}
+		}
+	}
+	minutes := 0
+	next := newQueue()
+	for q.len() > 0 {
+		cell, _ := q.dequeue()
+		for _, neighbor := range cell.neighbors() {
+			if !s.isInBounds(neighbor) || s.value(neighbor) != fresh {
+				continue
+			}
+			next.enqueue(neighbor)
+			s.set(neighbor, rotten)
+			freshCount--
+		}
+		if q.len() == 0 && next.len() > 0 {
+			q, next = next, q
+			minutes++
+		}
+	}
+	if freshCount > 0 {
+		return -1
+	}
+	return minutes
+}
+
+func (s *Solver) isInBounds(cell loc) bool {
+	return 0 <= cell.r && cell.r < s.m && 0 <= cell.c && cell.c < s.n
+}
+
+func (s *Solver) value(cell loc) int {
+	return s.grid[cell.r][cell.c]
+}
+
+func (s *Solver) set(cell loc, value int) {
+	s.grid[cell.r][cell.c] = value
+}
+
+const (
+	empty = iota
+	fresh
+	rotten
+)
+
+type loc struct {
+	r, c int
+}
+
+func (cell loc) neighbors() []loc {
+	return []loc{
+		{cell.r - 1, cell.c},
+		{cell.r + 1, cell.c},
+		{cell.r, cell.c - 1},
+		{cell.r, cell.c + 1},
+	}
+}
+
+type queue struct {
+	elems []loc
+}
+
+func newQueue() *queue {
+	return &queue{elems: []loc{}}
+}
+
+func (q *queue) enqueue(cell loc) {
+	q.elems = append(q.elems, cell)
+}
+
+func (q *queue) dequeue() (loc, bool) {
+	if q.len() == 0 {
+		return loc{}, false
+	}
+	cell := q.elems[0]
+	q.elems = q.elems[1:]
+	return cell, true
+}
+
+func (q *queue) len() int {
+	return len(q.elems)
+}
